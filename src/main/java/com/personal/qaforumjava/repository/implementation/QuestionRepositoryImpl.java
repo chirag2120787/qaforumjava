4fb2f2ebe3ef07fddf8ed7f70a2bdde7d30510c6
Chirag Bansal <cban@netlight.com>
2022-12-20 01:11:39 +0100

    remove repo implementation

--- src/main/java/com/personal/qaforumjava/repository/implementation/QuestionRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-//package com.personal.qaforumjava.repository.implementation;
-//
-//import com.personal.qaforumjava.repository.QuestionRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public class QuestionRepositoryImpl implements QuestionRepository { }
--- src/main/java/com/personal/qaforumjava/repository/implementation/UserRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-//package com.personal.qaforumjava.repository.implementation;
-//
-//import com.personal.qaforumjava.models.User;
-//import com.personal.qaforumjava.repository.UserRepository;
-//import java.util.Optional;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public class UserRepositoryImpl implements UserRepository {}
